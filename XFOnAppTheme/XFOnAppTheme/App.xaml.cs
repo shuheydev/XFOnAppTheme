53d0a1ce08156ac20865822bdc65adf447e8fb49
Shuhey <shuhey.dev@gmail.com>
2020-05-13 18:58:16 +0900

    Add project files.

---
+++ XFOnAppTheme/XFOnAppTheme/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFOnAppTheme
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Device.SetFlags(new List<string> {
+                "AppTheme_Experimental"
+            });
+
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
